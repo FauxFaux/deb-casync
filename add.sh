f233a3867b8cff97aff2198411696862b7470872
Chris West (Faux) <git@goeswhere.com>
2017-09-04 22:07:52 +0100

    actually maintain the execute permission

--- add.sh
+++ add.sh
@@ -2,12 +2,23 @@
 set -eu
 
 T=$(mktemp -d --suffix=sync)
-trap 'rm -rf '$T EXIT
+trap 'rm -rf '${T} EXIT
 
-dpkg-source --extract --no-check --no-copy --skip-debianization $2 $T/src >/dev/null
-cd $T/src
+# --no-check here skips signature checking, for performance. This is expected to be run on a local, trusted mirror.
+dpkg-source --extract --no-check --no-copy --skip-debianization $2 ${T}/src >/dev/null
+cd ${T}/src
 rm -rf debian
 
-# why are there unreadable files? combat-0.8.1
-chmod -R u+rX .
-casync make --without=2sec-time --without=usec-time --without=read-only --without=unix --without=best $1 >/dev/null
+# normalise permissions, to simulate git, and to ensure we can read files
+# this is, I believe, a common default umask.
+chmod -R u=rwX,g=rX,o=rX .
+
+# attempt to remove all metadata except permissions
+# check with make --verbose
+# see also https://github.com/systemd/casync/issues/81
+casync make \
+    --without=best \
+    --without=2sec-time --without=sec-time --without=usec-time \
+    --without=read-only \
+    --without=16bit-uids \
+    $1 >/dev/null
